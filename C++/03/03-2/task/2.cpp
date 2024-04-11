9f5748ac3e0fbd3af706fbbde08f988b59925aca
VERUS2621 <kim05190200@gmail.com>
2024-04-12 00:45:55 +0900

    수정

--- C++/03/03-2/task/2.cpp
+++ C++/03/03-2/task/2.cpp
@@ -13,8 +13,22 @@ void ShowString();
 
 void Printer::SetString(const char a[])
 {
-    strcpy(message, a);
+    strcpy_s(message, a); // strcpy를 사용하면 컴파일러가 비추천 함수를 사용할 때 경고를 출력함
 }
+
+// std::string은 C 스타일의 문자열보다 더 안전하고 편리하며, C++ 표준 라이브러리에서 제공함
+
+/*
+private:
+    std::string message; // std::string을 사용하여 문자열을 저장합니다.
+public:
+    void SetString(const std::string& a); // std::string을 인자로 받도록 변경합니다.
+
+void Printer::SetString(const std::string& a)
+{
+    message = a; // 간단히 대입 연산자를 사용하여 문자열을 복사합니다.
+}
+*/
 
 void Printer::ShowString()
 {
