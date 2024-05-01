371c49091f4dc0a067a8b3fbc701f492323d81f7
VERUS2621 <kim05190200@gmail.com>
2024-05-01 22:06:33 +0900

    과제 완

--- C++/sch/4.30.cpp
+++ C++/sch/4.30.cpp
@@ -7,18 +7,34 @@ public:
     Person(){ }
     Person(string name) { this->name = name; }
     string getName() {return name;}
-    void setName(string name); // 완성해야함
+    void setName(string name) { this->name = name; }
 };
 
 class Family {
     Person* p; // Person배열 포인터
     string familyName;
-    int size; // Person 배열 큭, 가족 구성원 수
+    int size; // Person 배열 크기, 가족 구성원 수
 public:
-    Family(string name, int size); // size 갯수만큼 Person 배열 동적 생성
-    void show();
-    void setName(int index, string name); // 완성해야함
-    ~Family();
+    Family(string name, int size) {
+        this->familyName=name;
+        this->size = size;
+        p = new Person[size];
+    } // size 갯수만큼 Person 배열 동적 생성
+    void show() // 가족의 이름 출력
+    {
+        cout << familyName << "가족은 다음과 같이 " << size << "명 입니다." << endl;
+        for ( int i = 0; i < size; i++) {
+            cout << p[i].getName() << "\t";
+        }
+    } 
+    void setName(int index, string name)
+    {
+        if (index >= 0 or index < size) {
+            p[index].setName(name);
+    }
+    }
+    ~Family()
+    { delete []p; }
 };
 
 int main() {
