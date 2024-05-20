ace592b5320c5287ba4c95a8701778674f808ec1
VERUS2621 <kim05190200@gmail.com>
2024-05-20 22:29:49 +0900

    .

--- C++/sch/5.14.cpp
+++ C++/sch/5.14.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
 class Person
@@ -7,41 +7,45 @@ class Person
     double weight;
     string name;
 
-public:
-    Person(int id=1, string name = "Grace", double weight = 20.5)
+public:    
+    Person(int id, string name) // 입력 매개변수 2개
+    {
+        this->id = id;
+        this->name = name;
+        this->weight = weight = 20.5;
+    }
+ 
+    Person(int id, string name, double weight) // 입력 매개변수 3개
     {
         this->id = id;
         this->name = name;
         this->weight = weight;
+    }
+    
+    Person() // 생성자
+    {
+        id = 1;
+        name = "Grace";
+        weight = 20.5;
     }
 
-    Person(int id, string name, double weight = 20.5)
-    {
-        this->id = id;
-        this->name = name;
-        this->weight = weight;
-    }
-
-    Person(int id, string name, double weight)
-    {
-        this->id = id;
-        this->name = name;
-        this->weight = weight;
-    }
-    /*Person(int a=1, string c = "grace", double b = 20.5)
+    /*
+    Person(int id=1, string name="Grace", double weight=20.5)
     {
         this->id = id;
         this->name = name;
         this->weight = weight;
     }
     */
-    void show() { cout << id << ' ' << weight << ' ' << name << endl;}
-};
 
-int main()
+    void show() { cout << id << ' ' << weight << ' ' << name << endl; }
+}; 
+
+int main() 
 {
     Person grace, ashley(2, "Ashley"), helen(3, "Hellen", 32.5);
     grace.show();
     ashley.show();
     helen.show();
-}
+    return 0;
+} 
